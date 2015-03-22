a6f7abde23b89065f9ee75847776b8d8862cb7f6
听寒 <10476912+pastrale@users.noreply.github.com>
2015-03-22 14:36:13 +0800

    fix and remove some invailed code

--- Lib/Action/MonsterinfoAction.class.php
+++ Lib/Action/MonsterinfoAction.class.php
@@ -8,9 +8,7 @@ class MonsterinfoAction extends Action {
     }
     public function data($id=NULL,$name=NULL,$star=NULL,$auto=NULL){
         $this->title = '精灵信息查询';
-        //$monster_info = M('monster_info');
         $monster_info = D('monster_info');
-        $emblem2 = M('emblem2');
         $this->ai = I('param.ai');
 		if($auto)
 		{
@@ -23,10 +21,8 @@ class MonsterinfoAction extends Action {
 		}
         if ($id or $name){
                 $this->lastfind = $id?$id:$name;
-                $condition = $id?"`ID`='{$id}'":"`DefName` LIKE '%{$name}%' AND ".($this->getAICondSQL());
+                $condition = $id?("`ID`='{$id}'"):("`DefName` LIKE '%{$name}%' AND ".($this->getAICondSQL()));
                 $this->monsters = $monster_info->Distinct(true)->alias('a')->where($condition)->join('JOIN seer2tool_emblem b ON a.NumbersID=b.NumbersID')->select();
-				//$moves = json_decode($this->monsters[0]['move']);
-                
         }else if($star){
                 $this->star = $star;
                 $this->monsters = $monster_info->where( "`StarLv`='{$star}' AND `EvolvesTo`='0' AND ".$this->getAICondSQL() )->order('ID desc')->select();
@@ -36,7 +32,6 @@ class MonsterinfoAction extends Action {
         }else{
 			$this->redirect('Monsterinfo/index', array(), 5, '未定义的操作');
 		}
-        //$monster_info->where("ID=$id")->limit(1)->select();
         //var_dump($this->monsters);
         $this->display();
     }
