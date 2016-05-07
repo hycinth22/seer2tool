d6dd65148530bfc9958a4f23c0ea54d859c05867
听寒 <10476912+pastrale@users.noreply.github.com>
2016-05-07 19:46:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('APP_NAME','Seer2 Information Tool');
 define('APP_PATH','./');
-define('APP_DEBUG',true); // 开启调试模式
+//define('APP_DEBUG',true); // 开启调试模式
 require './ThinkPHP/ThinkPHP.php';
 ?>
